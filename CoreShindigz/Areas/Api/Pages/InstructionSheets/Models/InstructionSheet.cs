75094f6293548f0edce2943ed3fed5e2dfc5c62c
PKraakman <pkraakman@shindigz.com>
2019-01-14 16:44:29 -0500

    Start of setup page

---
+++ CoreShindigz/Areas/Api/Pages/InstructionSheets/Models/InstructionSheet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShindigz.Areas.Api.Pages.InstructionSheets.Models
+{
+    public class InstructionSheet
+    {
+        private const string FolderPath = "";
+        public string Filename { get; set; }
+        public string Ext { get; set; }
+        public string UNCpath
+        {
+            get
+            {
+                return $@"{FolderPath}/{this.Filename}.{this.Ext}";
+            }
+        }
+    }
+}
---
+++ CoreShindigz/Areas/Api/Pages/InstructionSheets/setup.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoreShindigz.Areas.Api.Pages.InstructionSheets.Models;
+
+namespace CoreShindigz.Areas.Api.Pages.InstructionSheets
+{
+    public class SetupModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public List<InstructionSheet> InstructionSheets { get; set; }
+    }
+}
