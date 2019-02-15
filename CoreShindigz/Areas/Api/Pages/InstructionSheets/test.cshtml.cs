a5289a6472f999e1c12614ede5bc7064b830e4e7
PKraakman <pkraakman@shindigz.com>
2019-02-15 15:41:30 -0500

    Add security on test page

--- CoreShindigz/Areas/Api/Pages/InstructionSheets/test.cshtml.cs
+++ CoreShindigz/Areas/Api/Pages/InstructionSheets/test.cshtml.cs
@@ -4,20 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 
 using CoreShindigz.Areas.Api.Pages.InstructionSheets.v1.Models;
 
 namespace CoreShindigz.Areas.Api.Pages.InstructionSheets
 {
-    public class testModel : PageModel
+    [Authorize]
+    public class TestModel : PageModel
     {
         private InstructionSheetRepository _repo;
 
         public string FolderLocation { get; set; }
         public List<InstructionSheet> InstructionSheets { get; set; }
 
-        public testModel(IConfiguration configuration)
+        public TestModel(IConfiguration configuration)
         {
             _repo = new InstructionSheetRepository(configuration);
             FolderLocation = _repo.FolderPath;
